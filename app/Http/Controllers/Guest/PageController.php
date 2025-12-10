e5292d8f01f691f66f7a584df1795705729a3864
rolindaz <rolinda.zamboni@gmail.com>
2025-12-10 11:23:03 +0100

    feat: retrieve all movies in PageController index method

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Models\Movie;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
+        $movies = Movie::all();
         return view('home', compact('movies'));
     }
-};
+}
