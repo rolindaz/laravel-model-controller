8783f0dfb488198524149a51ce0f100c802dc894
rolindaz <rolinda.zamboni@gmail.com>
2025-12-10 11:29:42 +0100

    feat: add Vite for asset management in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel & Eloquent - Movies</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <h1>
