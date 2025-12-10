0e07368ad84384050b68f16cca9c2de604cdca98
rolindaz <rolinda.zamboni@gmail.com>
2025-12-10 11:11:50 +0100

    feat: create PageController and Movie model, set up home view and routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
